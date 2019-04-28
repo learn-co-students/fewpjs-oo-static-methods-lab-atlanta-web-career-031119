33d93d9fd1df1fd14769897c61ab8cb9f3a7aeaa
Darcy Linde <lindedarcy@gmail.com>
2019-04-28 15:25:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string){
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+    let result = [];
+    string.split(" ").forEach(function(w) {
+      if (!words.includes(w)){
+        result.push(Formatter.capitalize(w));
+      } else {
+        result.push(w);
+      }
+    });
+    return Formatter.capitalize(result.join(" "));
+  }
 }
