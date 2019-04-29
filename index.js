466816c7450ae261c60d506a353f41ac889587f5
Tyler Hueter <tylerhueter08@gmail.com>
2019-04-29 13:03:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'" "]+/g, '')
+  }
+
+  static titleize(string) {
+    let titleWords = string.split(" ");
+    let firstWord = titleWords.shift();
+    let articles = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newTitle = titleWords.map(function(word) {
+      if (articles.includes(word)) {
+         return word
+      } else {
+         return Formatter.capitalize(word)
+      }
+    });
+    
+    
+    return Formatter.capitalize(firstWord) + " "+ newTitle.join(" ")
+  }
+  
 }
